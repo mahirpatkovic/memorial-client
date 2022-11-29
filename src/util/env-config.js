ab0b2c826d555f119d89d2f97ef163ac6fd78e60
Mahir Patkovic <MPatkovic-ni@imax.com>
2022-11-29 10:41:19 +0100

    build changes

--- src/util/env-config.js
+++ src/util/env-config.js
@@ -1,6 +1,6 @@
 const ENVIRONMENTS = {
   dev: {
-    apiBase: 'http://localhost:4000',
+    apiBase: 'http://localhost:4000/api',
   },
   prod: {
     apiBase: 'ec2-3-121-22-149.eu-central-1.compute.amazonaws.com',

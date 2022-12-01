f3ed7795e2aa42b5d08052a35b0fd6351a8f24d4
Mahir Patkovic <MPatkovic-ni@imax.com>
2022-12-01 23:50:54 +0100

    added changes to https

--- src/util/env-config.js
+++ src/util/env-config.js
@@ -3,7 +3,7 @@ const ENVIRONMENTS = {
     apiBase: 'http://localhost:4000/api',
   },
   prod: {
-    apiBase: 'http://18.192.37.242:4000/api',
+    apiBase: 'https://18.192.37.242:4000/api',
   },
 };
 
